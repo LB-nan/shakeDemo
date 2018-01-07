33de9e726fccc191a577ca88e32a9ca6584a1ae2
oldpubcat <liangzhi21@qq.com>
2018-01-07 14:28:21 +0800

    测试

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 (function(){
 	var box = document.querySelector('#div');
 	var lastX,lastY,lastZ;
+	var maxRang = 80;
+	var minRang = 10;
+	var isShake = false;
 	// 记录用户摇一摇的幅度，然后对比前后两次摇的幅度判断用户是否在摇晃手机
 	window.addEventListener('devicemotion', function(e){
-		var motion = e.accelerationIncludingGravity;
 		var x = Math.round(motion.x);
 		var y = Math.round(motion.y);
 		var z = Math.round(motion.z);
-		var maxRang = 80;
-		var minRang = 15;
-		var isShake = false;
+		var motion = e.accelerationIncludingGravity;
 		if(typeof lastX == 'undefined'){
 			lastX = x;
 			lastY = y;
