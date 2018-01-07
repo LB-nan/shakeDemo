2bc257e30c3ce39aee06b2d2ba824b9a31db4112
oldpubcat <liangzhi21@qq.com>
2018-01-07 14:30:18 +0800

    修改

--- js/index.js
+++ js/index.js
@@ -1,34 +1,35 @@
 (function(){
-	var box = document.querySelector('#div');
-	var lastX,lastY,lastZ;
+	var lastX;
+	var lastY;
+	var lastZ;
 	var maxRang = 80;
 	var minRang = 10;
 	var isShake = false;
-	// 记录用户摇一摇的幅度，然后对比前后两次摇的幅度判断用户是否在摇晃手机
-	window.addEventListener('devicemotion', function(e){
+	var box = document.querySelector('#div');
+	window.addEventListener('devicemotion', function(e) {
+		var motion = e.accelerationIncludingGravity;
 		var x = Math.round(motion.x);
 		var y = Math.round(motion.y);
 		var z = Math.round(motion.z);
-		var motion = e.accelerationIncludingGravity;
-		if(typeof lastX == 'undefined'){
+		if(typeof lastX == "undefined"){
 			lastX = x;
 			lastY = y;
 			lastZ = z;
 			return;
 		}
-		
-		var dis = Math.abs(x - lastX) + Math.abs(y - lastY) + Math.abs(z - lastZ);
+		var dis = Math.abs(x - lastX) + Math.abs(y - lastY) +
+		Math.abs(z - lastZ);
 		if(dis > maxRang) {
 			isShake = true;
 		}
 		if(dis < minRang && isShake) {
 			isShake = false;
 			//执行 摇一摇之后，要操作的内容
-			box.innerHTML = dis;
+			alert("您晃动了手机");
 		}
+		box.innerHTML = dis;
 		lastX = x;
 		lastY = y;
 		lastZ = z;
-		
-	})
-})()
+	});
+})();
